c66a61ada0db6600710f5afc3e81558f41c50073
Bigadabman <korobovegor44@gmail.com>
2025-11-10 08:03:00 +0300

    added CPSAP 4

---
+++ CPSAP/Code/Lab04/04-02.js
@@ -0,0 +1,67 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var data_module = require('./DB_module');
+const PORT = 5000;
+
+var db = new data_module.DB();
+
+db.on('GET', async(request, response) => {
+    response.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
+    response.end(JSON.stringify(db.select()));
+})
+
+db.on('POST', async(request, response) => {
+    request.on('data', (data) => {
+        let dat = JSON.parse(data);
+        db.insert(dat);
+        response.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
+        response.end(JSON.stringify(db.select()));
+    })
+});
+
+
+db.on('PUT', async(request, response) => {
+    request.on('data', (data) => {
+        let dat = JSON.parse(data);
+        db.update(dat);
+        response.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
+        response.end(JSON.stringify(db.select()));
+    })
+});
+
+
+
+db.on('DELETE',async(request,response)=>{
+    let parsedURL = url.parse(request.url,true);
+    const id = parsedURL.query.id;
+    if(!id){
+        response.writeHead(400,{'content-type':'text/html;charset=utf-8'});
+        response.end('<h1>400 Bad request</h1>')
+    }
+    const deletedRow = db.delete(id);
+    response.writeHead(200,{'content-type':'application/json;charset=utf-8'});
+    response.end(JSON.stringify(deletedRow));
+});
+
+
+
+let server = http.createServer(function(request, response){
+    if(url.parse(request.url).pathname == '/api/db'){
+        db.emit(request.method, request, response);
+    }
+    else if(request.url=='/'){
+        var html = fs.readFileSync('index.html');
+        response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        response.end(html);
+    }
+    else{
+        response.writeHead(404,{'Content-Type':'text/html; charset=utf-8'});
+        response.end('<h1>404, Not found</h1>');
+    }
+
+});
+
+server.listen(PORT);
+
+console.log(`Server running at http://localhost:${PORT}/`);
